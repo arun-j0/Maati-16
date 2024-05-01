5344f9cee4093a269541183f3e4221fa01267b12
arun-j0 <arun2310kumar2002@gmail.com>
2024-05-01 17:49:27 +0530

    home update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,23 +16,24 @@ const HeroSection = () => {
       <div className="absolute top-0 z-10 w-full">
         <Navbar />
       </div>
-
+    
       {/* BottomBar */}
-      <div className="absolute z-10 w-full top-12">
+      <div className="absolute z-10 w-full top-20 md:top-24">
         <BottomBar />
       </div>
 
-      
       {/* Content */}
-      <div className="absolute inset-0 z-0 flex flex-col items-center justify-center text-center text-white ">
-        <h1 className="mb-8 text-4xl font-bold md:text-6xl font-outline-2 ">MAATI-16: Empowering Dreams</h1>
-        <p className="max-w-lg px-4 mb-8 text-lg font-bold md:text-2xl font-outline-1 ">
-          MAATI-16 is dedicated to providing education, mentorship, and resources to underprivileged children, empowering them to realize their dreams and create a better future. Our mission is to break the cycle of poverty through education and holistic support.
-        </p>
-        <p className="mb-8 text-xl italic font-bold md:text-3xl font-outline-1 ">"WE BELIEVE IN THE POWER OF EDUCATION TO TRANSFORM LIVES"</p>
-        <button className="px-8 py-3 font-bold text-gray-900 transition duration-300 ease-in-out bg-yellow-400 rounded-full hover:bg-yellow-500">
-          Contribute Now
-        </button>
+      <div className="absolute inset-0 flex items-center justify-center">
+        <div className="relative p-8 m-2 text-center bg-gray-300 top-12 md:top-24 md:w-2/3 lg:w-1/2 rounded-3xl bg-opacity-20 backdrop-blur-sm ">
+          <h1 className="mb-8 text-2xl font-bold text-transparent sm:text-4xl md:text-6xl bg-gradient-to-r bg-clip-text from-yellow-500 via-orange-500 to-amber-500 animate-text">MAATI-16: Empowering Dreams</h1>
+          <p className="mb-8 text-sm font-semibold text-transparent text-white sm:text-lg md:text-2xl">
+            MAATI-16 is dedicated to providing education, mentorship, and resources to underprivileged children, empowering them to realize their dreams and create a better future. Our mission is to break the cycle of poverty through education and holistic support.
+          </p>
+          <h2 className="mb-8 text-lg italic font-bold text-transparent sm:text-xl md:text-3xl bg-gradient-to-r bg-clip-text from-yellow-500 via-orange-500 to-amber-500 animate-text">"WE BELIEVE IN THE POWER OF EDUCATION TO TRANSFORM LIVES"</h2>
+          <button className="px-8 py-3 font-bold text-gray-900 transition duration-300 ease-in-out bg-yellow-400 rounded-full hover:bg-yellow-500">
+            Contribute Now
+          </button>
+        </div>
       </div>
     </div>
   );
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,26 +1,62 @@
-import React, { useState } from 'react';
-import { FaHome, FaInfoCircle, FaImages, FaCalendarAlt, FaBlog, FaPhone, FaBars, FaTimes } from 'react-icons/fa';
+import React, { useState, useRef } from 'react';
+import { FaHome, FaInfoCircle, FaImages, FaCalendarAlt, FaBlog, FaPhone, FaBars, FaTimes, FaAngleDown, FaUserPlus } from 'react-icons/fa';
 import { MdContactSupport } from "react-icons/md";
 import Logo from '../assets/logo.png';
 
 const Navbar = () => {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+  const [isAboutOpen, setIsAboutOpen] = useState(false);
+  const [isCareersOpen, setIsCareersOpen] = useState(false);
+  const aboutTimeout = useRef(null);
+  const careersTimeout = useRef(null);
 
   const toggleMobileMenu = () => {
     setIsMobileMenuOpen(!isMobileMenuOpen);
   };
 
-  const closeMobileMenu = () => {
+  const toggleAboutMenu = () => {
+    setIsAboutOpen(!isAboutOpen);
+    setIsCareersOpen(false);
+  };
+
+  const toggleCareersMenu = () => {
+    setIsCareersOpen(!isCareersOpen);
+    setIsAboutOpen(false);
+  };
+
+  const closeMenus = () => {
+    setIsAboutOpen(false);
+    setIsCareersOpen(false);
     setIsMobileMenuOpen(false);
   };
 
- 
+  const handleAboutMouseEnter = () => {
+    clearTimeout(aboutTimeout.current);
+    setIsAboutOpen(true);
+  };
+
+  const handleAboutMouseLeave = () => {
+    aboutTimeout.current = setTimeout(() => {
+      setIsAboutOpen(false);
+    }, 1000); // Delay time in milliseconds
+  };
+
+  const handleCareersMouseEnter = () => {
+    clearTimeout(careersTimeout.current);
+    setIsCareersOpen(true);
+  };
+
+  const handleCareersMouseLeave = () => {
+    careersTimeout.current = setTimeout(() => {
+      setIsCareersOpen(false);
+    }, 1000); // Delay time in milliseconds
+  };
 
   return (
     <div className="flex items-center justify-between py-2 text-white">
       {/* Logo */}
-      <div className="mx-4 md:mx-20">
-        <img src={Logo} alt="Logo" className="w-auto h-8" />
+      <div className="mx-4 mt-3 md:mt-auto md:mx-20">
+        <img src={Logo} alt="Logo" className="w-auto h-14 md:h-20" />
       </div>
 
       {/* Mobile navigation toggle */}
@@ -35,16 +71,43 @@ const Navbar = () => {
       {/* Mobile navigation menu */}
       {isMobileMenuOpen && (
         <div className="absolute top-0 left-0 flex flex-col items-center justify-center w-full h-screen px-2 py-4 font-bold text-white bg-gray-800 md:hidden opacity-80">
-          <FaTimes onClick={closeMobileMenu} className="absolute text-white cursor-pointer top-4 right-4" />
+          <FaTimes onClick={closeMenus} className="absolute text-white cursor-pointer top-8 right-4" />
           <div className="flex flex-col items-center space-y-4">
-            <a href="/" className="flex items-center hover:text-gray-300" onClick={closeMobileMenu}><FaHome className="mr-1" />Home</a>
-            <a href="/about" className="flex items-center hover:text-gray-300" onClick={closeMobileMenu}><FaInfoCircle className="mr-1" />About Us</a>
-            <a href="/gallery" className="flex items-center hover:text-gray-300" onClick={closeMobileMenu}><FaImages className="mr-1" />Gallery</a>
-            <a href="/events" className="flex items-center hover:text-gray-300" onClick={closeMobileMenu}><FaCalendarAlt className="mr-1" />Events</a>
-            <a href="/blog" className="flex items-center hover:text-gray-300" onClick={closeMobileMenu}><FaBlog className="mr-1" />Blog</a>
-            <a href="/contact" className="flex items-center hover:text-gray-300" onClick={closeMobileMenu}><MdContactSupport className="mr-1" />Contact Us</a>
-            <button className="bg-white text-[#991b1b] py-2 px-4 rounded-lg hover:bg-gray-300" onClick={(e) => { handleDonateClick(e); closeMobileMenu(); }}>
-              Donate Now
+            <a href="/" className="flex items-center hover:text-[#991b1b] link link-underline link-underline-black" onClick={closeMenus}><FaHome className="mr-1" />Home</a>
+            <div className="relative group" onMouseEnter={handleAboutMouseEnter} onMouseLeave={handleAboutMouseLeave}>
+              <button onClick={toggleAboutMenu} className="flex items-center hover:text-gray-300">
+                <FaInfoCircle className="mr-1" /> About Us <FaAngleDown className="ml-1" />
+              </button>
+              {isAboutOpen && (
+                <div className="absolute left-0 z-10 p-4 mt-2 bg-gray-800 rounded-lg top-full">
+                  <div style={{ display: 'flex', flexDirection: 'column', gap: '0.5rem' }}>
+                    <a href="/personal-details" className="flex items-center text-white hover:text-[#991b1b] link link-underline link-underline-black"><FaUserPlus className="mr-2" /> Personal Details</a>
+                    <a href="/core-members" className="flex items-center text-white hover:text-[#991b1b] link link-underline link-underline-black"><FaUserPlus className="mr-2" /> Core Members</a>
+                    <a href="/motto-and-objective" className="flex items-center text-white hover:text-[#991b1b] link link-underline link-underline-black"><FaUserPlus className="mr-2" /> Our Motto and Objective</a>
+                    <a href="/faqs-policies" className="flex items-center text-white hover:text-[#991b1b] link link-underline link-underline-black"><FaUserPlus className="mr-2" /> FAQs and Policies</a>
+                  </div>
+                </div>
+              )}
+            </div>
+            <div className="relative group" onMouseEnter={handleCareersMouseEnter} onMouseLeave={handleCareersMouseLeave}>
+              <button onClick={toggleCareersMenu} className="flex items-center hover:text-gray-300">
+                <MdContactSupport className="mr-1" /> Careers <FaAngleDown className="ml-1" />
+              </button>
+              {isCareersOpen && (
+                <div className="absolute left-0 z-10 p-4 mt-2 bg-gray-800 rounded-lg top-full">
+                  <div style={{ display: 'flex', flexDirection: 'column', gap: '0.5rem' }}>
+                    <a href="/membership-form" className="flex items-center text-white hover:text-[#991b1b]"><MdContactSupport className="mr-2" /> Membership/Volunteer Form</a>
+                    <a href="/jobs-internships" className="flex items-center text-white hover:text-[#991b1b]"><MdContactSupport className="mr-2" /> Jobs/Internships</a>
+                  </div>
+                </div>
+              )}
+            </div>
+            <a href="/gallery" className="flex items-center hover:text-[#991b1b]" onClick={closeMenus}><FaImages className="mr-1" /> Gallery</a>
+            <a href="/events" className="flex items-center hover:text-[#991b1b]" onClick={closeMenus}><FaCalendarAlt className="mr-1" /> Events</a>
+            <a href="/blog" className="flex items-center hover:text-[#991b1b]" onClick={closeMenus}><FaBlog className="mr-1" /> Blog</a>
+            <a href="/contact" className="flex items-center hover:ttext-[#991b1b]" onClick={closeMenus}><MdContactSupport className="mr-1" /> Contact Us</a>
+            <button className="bg-white text-[#991b1b] py-2 px-4 rounded-lg hover:bg-gray-300" onClick={closeMenus}>
+              Become a Member
             </button>
           </div>
         </div>
@@ -52,13 +115,39 @@ const Navbar = () => {
 
       {/* Normal navigation for larger devices */}
       <div className="justify-end flex-grow hidden mx-4 md:flex md:mx-20">
-        <nav className="flex items-center space-x-4">
-          <a href="/" className="flex items-center hover:text-gray-300"><FaHome className="mr-1" /> Home</a>
-          <a href="/about" className="flex items-center hover:text-gray-300"><FaInfoCircle className="mr-1" /> About Us</a>
-          <a href="/gallery" className="flex items-center hover:text-gray-300"><FaImages className="mr-1" /> Gallery</a>
-          <a href="/events" className="flex items-center hover:text-gray-300"><FaCalendarAlt className="mr-1" /> Events</a>
-          <a href="/blog" className="flex items-center hover:text-gray-300"><FaBlog className="mr-1" /> Blog</a>
-          <a href="/contact" className="flex items-center hover:text-gray-300"><MdContactSupport className="mr-1" /> Contact Us</a>
+        <nav className="flex items-center space-x-4 text-lg">
+          <a href="/" className="flex items-center hover:text-[#991b1b] link link-underline link-underline-black"><FaHome className="mr-1" /> Home</a>
+          <div className="relative group" onMouseEnter={handleAboutMouseEnter} onMouseLeave={handleAboutMouseLeave}>
+            <button onClick={toggleAboutMenu} className="flex items-center hover:text-[#991b1b] link link-underline link-underline-black">
+              <FaInfoCircle className="mr-1" /> About Us <FaAngleDown className="ml-1" />
+            </button>
+            {isAboutOpen && (
+              <div className="absolute z-10 w-64 p-4 mt-2 bg-gray-800 rounded-lg top-full opacity-80">
+                <a href="/personal-details" className="flex items-center text-white hover:text-[#991b1b] link link-underline link-underline-black"><FaUserPlus className="mr-2" /> Personal Details</a>
+                <a href="/core-members" className="flex items-center text-white hover:text-[#991b1b] link link-underline link-underline-black"><FaUserPlus className="mr-2" /> Core Members</a>
+                <a href="/motto-and-objective" className="flex items-center text-white hover:text-[#991b1b] link link-underline link-underline-black"><FaUserPlus className="mr-2" /> Our Motto and Objective</a>
+                <a href="/faqs-policies" className="flex items-center text-white hover:text-[#991b1b] link link-underline link-underline-black"><FaUserPlus className="mr-2" /> FAQs and Policies</a>
+              </div>
+            )}
+          </div>
+          <div className="relative group" onMouseEnter={handleCareersMouseEnter} onMouseLeave={handleCareersMouseLeave}>
+            <button onClick={toggleCareersMenu} className="flex items-center hover:text-[#991b1b] link link-underline link-underline-black">
+              <MdContactSupport className="mr-1" /> Careers <FaAngleDown className="ml-1" />
+            </button>
+            {isCareersOpen && (
+              <div className="absolute z-10 p-4 mt-2 bg-gray-800 rounded-lg top-full opacity-80 w-[300px]">
+                <a href="/membership-form" className="flex items-center text-white hover:text-[#991b1b] link link-underline link-underline-black"><MdContactSupport className="mr-2" /> Membership/Volunteer Form</a>
+                <a href="/jobs-internships" className="flex items-center text-white hover:text-[#991b1b] link link-underline link-underline-black"><MdContactSupport className="mr-2" /> Jobs/Internships</a>
+              </div>
+            )}
+          </div>
+          <a href="/gallery" className="flex items-center hover:text-[#991b1b] link link-underline link-underline-black"><FaImages className="mr-1" /> Gallery</a>
+          <a href="/events" className="flex items-center hover:text-[#991b1b] link link-underline link-underline-black"><FaCalendarAlt className="mr-1" /> Events</a>
+          <a href="/blog" className="flex items-center hover:text-[#991b1b] link link-underline link-underline-black"><FaBlog className="mr-1" /> Blog</a>
+          <a href="/contact" className="flex items-center hover:text-[#991b1b] link link-underline link-underline-black"><MdContactSupport className="mr-1" /> Contact Us</a>
+          <button className="bg-white text-[#991b1b] py-2 px-4 rounded-lg hover:bg-gray-300 link link-underline link-underline-black">
+            Become a Member
+          </button>
         </nav>
       </div>
     </div>
