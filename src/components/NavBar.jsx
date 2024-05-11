9e0e1e7e9c365f4f5fbec9739fd1884f7f17aaa0
VaidehiRewale <139336081+VaidehiRewale@users.noreply.github.com>
2024-05-11 08:14:55 +0530

    NavBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,8 +5,8 @@ import { GiHamburgerMenu } from 'react-icons/gi';
 
 const Navbar = () => {
     const [isOpen, setIsOpen] = useState(false);
-    const [showForm, setShowForm] = useState(false); // State to track whether to show the form or not
-    const [showFAQs, setShowFAQs] = useState(false); // State to track whether to show FAQs or not
+    const [showForm, setShowForm] = useState(false); 
+    const [showFAQs, setShowFAQs] = useState(false); 
 
     const toggleMenu = () => {
         setIsOpen(!isOpen);
@@ -28,7 +28,6 @@ const Navbar = () => {
         <nav className="bg-[#280101] text-[#fee57e]">
             <div className="container px-4 mx-auto">
                 <div className="flex items-center justify-between py-4">
-                    {/* Hamburger Menu - Mobile */}
                     <div className="right-0 flex-grow text-right md:hidden">
                         <button onClick={toggleMenu} className=" text-white hover:text-[#fee57e]  focus:outline-none">
                             <GiHamburgerMenu className="w-6 h-6" />
@@ -90,7 +89,6 @@ const Navbar = () => {
                             </div>
                         )}
                     </div>
-                    {/* Menu - Desktop */}
                     <div className="hidden w-full md:flex md:items-center md:justify-center md:space-x-4">
                         <ul className="flex space-x-4">
                             <li>
@@ -104,6 +102,7 @@ const Navbar = () => {
                                 <div className="absolute left-0 w-48 py-2 mt-2 transition-opacity duration-300 bg-[#280101] rounded-md shadow-lg opacity-0 top-full group-hover:opacity-100 border-[#fee57e] border-2">
                                     <a href="#" className="block px-4 py-2 text-gray-300 hover:text-white">Personal Details</a>
                                     <a href="#" className="block px-4 py-2 text-gray-300 hover:text-white">Core Members</a>
+                                    <a href="#" className="block px-4 py-2 text-2xl text-left text-gray-300 hover:text-white">Our Motto and Objective</a>
                                     <a href="#" className="block px-4 py-2 text-gray-300 hover:text-white" onClick={toggleFAQs}>FAQs and Policies</a>
                                 </div>
                             </li>
@@ -141,7 +140,6 @@ const Navbar = () => {
                     </div>
                 </div>
             </div>
-            {/* Form Modal */}
             {showForm && (
                 <div className="fixed top-0 left-0 z-50 flex items-center justify-center w-full h-full bg-black bg-opacity-50">
                     <div className="bg-[#6e4019] text-[#fee57e] p-8 rounded-md shadow-lg max-h-[80vh] overflow-auto form-container">
@@ -255,67 +253,30 @@ const Navbar = () => {
                     </div>
                 </div>
             )}
-
-            {/* FAQs Modal */}
             {showFAQs && (
                 <div className="fixed top-0 left-0 z-50 flex items-center justify-center w-full h-full bg-black bg-opacity-50">
                     <div className="bg-[#6e4019] text-[#fee57e] p-8 rounded-md shadow-lg max-h-[80vh] overflow-auto form-container">
                         <h2 className="text-xl font-semibold mb-4">FAQs and Policies</h2>
-                        <div className="mb-4">
-                            <h3 className="text-lg font-semibold mb-2 text-[#fee57e]">Frequently Asked Questions</h3>
-                            <ul className="list-disc pl-6">
-                                <li className="mb-2">
-                                    <span className="font-semibold text-[#fee57e]">Q: </span> What is the mission of Maati 16 Trust?
-                                </li>
-                                <li className="mb-2">
-                                    <span className="font-semibold text-[#fee57e]">A: </span> Maati 16 is an organization dedicated to providing education and reducing the illiteracy rate. The mission of Maati 16 is to empower individuals through education, with a focus on reducing the illiteracy rate in India. We strive to provide accessible and quality education to underserved populations, including children, adults, and marginalized communities. Through innovative programs and partnerships, we aim to equip individuals with the knowledge, skills, and opportunities they need to thrive in today's world. By addressing the root causes of illiteracy and promoting lifelong learning, we envision a future where every person has the chance to reach their full potential and contribute positively to society.
-                                </li>
-                                <li className="mb-2">
-                                    <span className="font-semibold text-[#fee57e]">Q: </span> How old is Maati 16 Trust?
-                                </li>
-                                <li className="mb-2">
-                                    <span className="font-semibold text-[#fee57e]">A: </span> Maati 16 was established in the year 2016.
-                                </li>
-                                <li className="mb-2">
-                                    <span className="font-semibold text-[#fee57e]">Q: </span> Which Areas does Maati 16 Trust works in?
-                                </li>
-                                <li className="mb-2">
-                                    <span className="font-semibold text-[#fee57e]">A: </span> Maati 16 has been working in the thematic areas of education, health care, youth employability and empowerment of women and girls. Education is our priority.
-                                </li>
-                                <li className="mb-2">
-                                    <span className="font-semibold text-[#fee57e]">Q: </span> How to become a Volunteer in Maati 16 Trust?
-                                </li>
-                                <li className="mb-2">
-                                    <span className="font-semibold text-[#fee57e]">A: </span> You need to fill the Membership/Volunteer form of Maati 16. It will take 24 – 48 hours to verify and then you receive an approval email.
-                                </li>
-                                <li className="mb-2">
-                                    <span className="font-semibold text-[#fee57e]">Q: </span> How can I donate to Maati 16 Trust?
-                                </li>
-                                <li className="mb-2">
-                                    <span className="font-semibold text-[#fee57e]">A: </span> You can go to “Donate Now” section of Maati 16 Trust’s Website and donate online. For other donations related queries you can please write to maati16officail@gmail.com
-                                </li>
-                                <li className="mb-2">
-                                    <span className="font-semibold text-[#fee57e]">Q: </span> What is project Gyan Sagar?
-                                </li>
-                                <li className="mb-2">
-                                    <span className="font-semibold text-[#fee57e]">A: </span> Our primary focus is child education. In this project we help the needy children who want to study and make their future bright. We want to reduce the illiteracy rate of India. We are working in Bihar and Delhi. We want to cover all states of India. We also help the orphanages and provide study and stationary materials to them, we are connected with 10 – 15 Orphanages till date. This number will increase soon.
-                                </li>
-                                <li className="mb-2">
-                                    <span className="font-semibold text-[#fee57e]">Q: </span> How can I work form Maati 16?
-                                </li>
-                                <li className="mb-2">
-                                    <span className="font-semibold text-[#fee57e]">A: </span> Please go to our “Carrer” page to know more about the current openings.
-                                </li>
-                            </ul>
+                        <div className="space-y-4">
+                            <div className="border-b border-[#fee57e] pb-4">
+                                <h3 className="text-lg font-semibold">FAQ 1</h3>
+                                <p className="text-sm">Answer 1</p>
+                            </div>
+                            <div className="border-b border-[#fee57e] pb-4">
+                                <h3 className="text-lg font-semibold">FAQ 2</h3>
+                                <p className="text-sm">Answer 2</p>
+                            </div>
+                            <div className="border-b border-[#fee57e] pb-4">
+                                <h3 className="text-lg font-semibold">FAQ 3</h3>
+                                <p className="text-sm">Answer 3</p>
+                            </div>
                         </div>
-                        <div className="mt-4 text-right">
-                            <button
-                                className="bg-[#fee57e] hover:bg-yellow-400 text-gray-800 font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                                onClick={toggleFAQs}
-                            >
-                                Close
-                            </button>
-                        </div>
+                        <button
+                            className="absolute top-4 right-4 bg-transparent text-[#fee57e] hover:text-white focus:outline-none"
+                            onClick={toggleFAQs}
+                        >
+                            <FaTimes className="w-6 h-6" />
+                        </button>
                     </div>
                 </div>
             )}
