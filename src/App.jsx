c0dc3170e92a6ce571d42c2e36a5ee5226c27112
VaidehiRewale <139336081+VaidehiRewale@users.noreply.github.com>
2024-08-05 14:22:11 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Updated import for v6 or above
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import BlogPage from './pages/BlogPage';
 import BlogDetailPage from './pages/BlogDetailPage';
@@ -7,20 +7,21 @@ import WorkPage from './pages/WorkPage';
 import DetailedWorkPage from './pages/DetailedWorkPage';
 import GalleryPage from './pages/GalleryPage';
 import DonationPage from './pages/DonationPage';
-
+import HowYouCanHelp from './components/HowYouCanHelp';
 
 const App = () => {
   return (
     <Router>
-      <div>   
+      <div>
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/blogs" element={<BlogPage />} />
-          <Route path="/blogs/:id" element={<BlogDetailPage/>}/>
+          <Route path="/blogs/:id" element={<BlogDetailPage />} />
           <Route path="/works" element={<WorkPage />} />
-          <Route path='/works/:id' element={<DetailedWorkPage/>} />
-          <Route path="/gallery" element={<GalleryPage/>}/>
-          <Route path="/donation" element={<DonationPage/>}/>
+          <Route path='/works/:id' element={<DetailedWorkPage />} />
+          <Route path="/gallery" element={<GalleryPage />} />
+          <Route path="/donation" element={<DonationPage />} />
+          <Route path="/how-you-can-help" element={<HowYouCanHelp />} />
         </Routes>
       </div>
     </Router>
